99f0f8c484b77bd317f8792409755bc5914eaf0f
Christoph Mertins <c.mertins@gmail.com>
2018-10-16 20:33:18 +0200

    cleanup

--- src/main/java/cm/homeautomation/entities/Camera.java
+++ src/main/java/cm/homeautomation/entities/Camera.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlTransient;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
