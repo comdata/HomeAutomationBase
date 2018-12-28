e67609eb073ed04ec468a1eb6f859a593997663d
Christoph Mertins <c.mertins@gmail.com>
2018-12-28 17:51:24 +0100

    add JPA annotations

--- src/main/java/cm/homeautomation/entities/Sensor.java
+++ src/main/java/cm/homeautomation/entities/Sensor.java
@@ -36,7 +36,9 @@ public class Sensor {
 	private String sensorPosition = "LOCAL";
 	private int deadbandPercent = 0;
 	
+	@Column(name = "MINVALUE", nullable = true)
 	private String minValue=null;
+	@Column(name = "MAXVALUE", nullable = true)
 	private String maxValue=null;
 
 	@Column(name = "SHOWDATA")
