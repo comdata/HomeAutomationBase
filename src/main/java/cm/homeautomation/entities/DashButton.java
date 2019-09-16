de3a52d4e0cd1327ca46ef4539b05200eb569720
Christoph Mertins <c.mertins@gmail.com>
2019-09-16 14:11:48 +0200

    add xmlrootelement

--- src/main/java/cm/homeautomation/entities/DashButton.java
+++ src/main/java/cm/homeautomation/entities/DashButton.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
@@ -14,6 +15,7 @@ import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @Entity
+@XmlRootElement
 public class DashButton {
 
 	@Id
